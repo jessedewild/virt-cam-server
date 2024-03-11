a06a1d98d80dc83bcd8921fd4de492a68129fa1a
Jesse de Wild <jessedw@outlook.com>
2024-03-11 15:34:17 +0100

    Spawn command for executing whip client

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 const cors = require('cors');
 const https = require('https');
 const fs = require('fs');
@@ -18,6 +18,9 @@ const credentials = { key: privateKey, cert: certificate };
 
 let whipServerUrl = null;
 let streamingRoom = null;
+
+let boardCmd = null;
+let playerCmd = null;
 
 app.post('/start', async (req, res) => {
   const { whip_server_url, room, board_cam_display, player_cam_display } = req.body;
@@ -48,20 +51,73 @@ app.post('/start', async (req, res) => {
     // Store the room
     streamingRoom = room;
 
-    // Execute child_process commands
-    exec(
-      `./simple-whip-client/whip-client -u ${whipServerUrl}/endpoint/${room}board -V "v4l2src device=/dev/video0 ! video/x-raw,width=960,height=720,framerate=30/1 ! videoconvert ! queue ! x264enc tune=zerolatency bitrate=1500 speed-preset=ultrafast ! rtph264pay config-interval=5 pt=96 ssrc=1 ! queue ! application/x-rtp,media=video,encoding-name=H264,payload=96"`
-    );
+    setTimeout(() => {
+      boardCmd = spawn(
+        './simple-whip-client/whip-client',
+        [
+          '-u',
+          `${whipServerUrl}/endpoint/${room}board`,
+          '-V',
+          '"v4l2src device=/dev/video0 ! video/x-raw,width=960,height=720,framerate=30/1 ! videoconvert ! queue ! x264enc tune=zerolatency bitrate=1500 speed-preset=ultrafast ! rtph264pay config-interval=5 pt=96 ssrc=1 ! queue ! application/x-rtp,media=video,encoding-name=H264,payload=96"',
+        ],
+        {
+          shell: true,
+        }
+      );
+      boardCmd.stdout.on('data', (data) => {
+        console.log(`[BOARD]: ${data}`);
+      });
+      boardCmd.stderr.on('data', (data) => {
+        console.error(`[BOARD]: ${data}`);
+      });
+      boardCmd.on('close', (code) => {
+        console.log(`Board cam process exited with code ${code}`);
+      });
 
-    exec(
-      `./simple-whip-client/whip-client -u ${whipServerUrl}/endpoint/${room}player -V "v4l2src device=/dev/video1 ! video/x-raw,width=960,height=720,framerate=30/1 ! videoconvert ! queue ! x264enc tune=zerolatency bitrate=1500 speed-preset=ultrafast ! rtph264pay config-interval=5 pt=96 ssrc=2 ! queue ! application/x-rtp,media=video,encoding-name=H264,payload=96"`
-    );
+      playerCmd = spawn(
+        './simple-whip-client/whip-client',
+        [
+          '-u',
+          `${whipServerUrl}/endpoint/${room}player`,
+          '-V',
+          '"v4l2src device=/dev/video1 ! video/x-raw,width=960,height=720,framerate=30/1 ! videoconvert ! queue ! x264enc tune=zerolatency bitrate=1500 speed-preset=ultrafast ! rtph264pay config-interval=5 pt=96 ssrc=2 ! queue ! application/x-rtp,media=video,encoding-name=H264,payload=96"',
+        ],
+        {
+          shell: true,
+        }
+      );
+      playerCmd.stdout.on('data', (data) => {
+        console.log(`[PLAYER]: ${data}`);
+      });
+      playerCmd.stderr.on('data', (data) => {
+        console.error(`[PLAYER]: ${data}`);
+      });
+      playerCmd.on('close', (code) => {
+        console.log(`Player cam process exited with code ${code}`);
+      });
+    }, 2000);
 
     res.sendStatus(200);
   } catch (error) {
     console.error('Error making POST requests or running commands:', error);
     res.status(500).send('An error occurred');
   }
+});
+
+app.get('/stop', (req, res) => {
+  streamingRoom = null;
+
+  if (boardCmd) {
+    boardCmd.kill();
+    boardCmd = null;
+  }
+
+  if (playerCmd) {
+    playerCmd.kill();
+    playerCmd = null;
+  }
+
+  res.sendStatus(200);
 });
 
 app.get('/status', (req, res) => {
